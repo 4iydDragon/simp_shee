35bed8277d8025aa4baaa84487beba62320709ce
Emmanuel.lvg <emmanuel.k.kangogo@gmail.com>
2023-05-26 08:51:31 +0000

    full argm

--- old/prompt.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void prompt() {
-    printf("Shell > ");
-}
--- old/stat.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-void check_status(int status, char* command) {
-    if (WIFEXITED(status) && WEXITSTATUS(status) == 1) {
-        fprintf(stderr, "Command execution failed: %s\n", command);
-    }
-}
